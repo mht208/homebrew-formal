2eba00caf448faf58ec1e3a4d46fdb0e545163e3
Ming-Hsien Tsai <mhtsai208@gmail.com>
2012-09-17 20:58:47 +0800

    Add an option to ocamlgraph to build native binaries and libraries, and fix a bug in installing documentations.

--- ocamlgraph.rb
+++ ocamlgraph.rb
@@ -5,6 +5,7 @@ class Ocamlgraph < Formula
   url 'http://ocamlgraph.lri.fr/download/ocamlgraph-1.8.2.tar.gz'
   sha1 'ff864aaae11f7355685f37815194ca58d3fcac01'
 
+  option 'with-opt', 'Build native binaries and libraries'
   option 'with-doc', 'Install documentations'
 
   depends_on 'objective-caml'
@@ -16,9 +17,11 @@ class Ocamlgraph < Formula
 
     system "./configure --prefix=#{prefix}"
     system "make"
+    system "make opt" if build.include? 'with-opt'
     system "make doc" if build.include? 'with-doc'
     system "make install OCAMLLIB=#{prefix_stdlib}"
+    system "make install-opt  OCAMLLIB=#{prefix_stdlib}" if build.include? 'with-opt'
     cp "META", "#{prefix_stdlib}/ocamlgraph"
-    doc.install (Dir.glob "doc/*")
+    doc.install (Dir.glob "doc/*") if build.include? 'with-doc'
   end
 end
