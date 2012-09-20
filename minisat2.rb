2c5acd87cb605124716b765fadafa428b320a8d3
Ming-Hsien Tsai <mhtsai208@gmail.com>
2012-09-20 19:00:37 +0800

    Install all headers of minisat2 and add an option to build with simplification capabilities.

--- minisat2.rb
+++ minisat2.rb
@@ -5,12 +5,17 @@ class Minisat2 < Formula
   url 'http://minisat.se/downloads/minisat2-070721.zip'
   sha1 'cb4a58f8a8192a65b1b944c4307afdf029d51e1c'
 
+  option 'with-simp', 'Build with simplification capabilities'
+
   def install
-    Dir.chdir 'core' do
+    d = (build.include? 'with-simp') ? 'simp' : 'core'
+    Dir.chdir d do
       system "make r lib CXX=g++"
       bin.install 'minisat_release' => 'minisat2'
       lib.install 'libminisat.a' => 'libminisat2.a'
     end
-    (include/'minisat2').install Dir['mtl/*.h'], 'core/Solver.h'
+    (include/'minisat2/core').install 'core/Solver.h'
+    (include/'minisat2/mtl').install Dir['mtl/*.h']
+    (include/'minisat2/simp').install 'simp/SimpSolver.h'
   end
 end
