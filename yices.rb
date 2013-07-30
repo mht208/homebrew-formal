1f312dcc26a5839fbcb76fbeaf5f7e7d82b8d08f
Ming-Hsien Tsai <mhtsai208@gmail.com>
2013-07-30 21:49:23 +0800

    Update Yices to 1.0.38.

--- yices.rb
+++ yices.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class YicesDownloadStrategy < CurlDownloadStrategy
   def _fetch
-    if not File.exists? "#{HOMEBREW_CACHE}/yices-1.0.36-x86_64-apple-darwin10.8.0-static-gmp.tar.gz"
+    if not File.exists? "#{HOMEBREW_CACHE}/yices-1.0.38-x86_64-apple-darwin10.8.0-static-gmp.tar.gz"
       onoe <<-EOS.undent
       Due to license restrictions, we cannot fetch the distributables
       automagically.
 
-      1. Visit http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.36-x86_64-apple-darwin10.8.0-static-gmp.tar.gz and click 'I accept'.
+      1. Visit http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.38-x86_64-apple-darwin10.8.0-static-gmp.tar.gz and click 'I accept'.
       2. Move file to #{HOMEBREW_CACHE}
-      3. Rename the tarball to yices-1.0.36.tar.gz.
+      3. Rename the tarball to yices-1.0.38.tar.gz.
       4. Install this package again.
       EOS
     end
@@ -18,9 +18,9 @@ end
 
 class Yices < Formula
   homepage 'http://yices.csl.sri.com'
-  url 'http://yices.csl.sri.com/yices-1.0.36-x86_64-apple-darwin10.8.0-static-gmp.tar.gz',
+  url 'http://yices.csl.sri.com/yices-1.0.38-x86_64-apple-darwin10.8.0-static-gmp.tar.gz',
       :using => YicesDownloadStrategy
-  sha1 'fd3518966fd3351ee6803ae1b648daa876aa7427'
+  sha1 '686e45f2ad5e3fefa3c4c795d12c775b8f333189'
 
   def install
     bin.install "bin/yices"
