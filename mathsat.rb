7ae1334a1eb626632fbcef1995082d817a481736
Ming-Hsien Tsai <mhtsai208@gmail.com>
2013-07-31 15:15:29 +0800

    Update mathsat to 5.2.8.

--- mathsat.rb
+++ mathsat.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mathsat < Formula
   homepage 'http://mathsat.fbk.eu/index.html'
-  url 'http://mathsat.fbk.eu/download.php?file=mathsat-5.1.12-darwin-x86_64.tar.gz'
-  version '5.1.12'
-  sha1 '3f34baef5c4cdde912b69ddf2f55f24975cf8062'
+  url 'http://mathsat.fbk.eu/download.php?file=mathsat-5.2.8-darwin-libcxx-x86_64.tar.gz'
+  version '5.2.8'
+  sha1 'b89776c4648c0d4f8a503c4757e15dfa0964f6f6'
 
   def install
     bin.install 'bin/mathsat'
