87f66b962e9bf08bcf6b5fae1019696b10a1528e
Ming-Hsien Tsai <mhtsai208@gmail.com>
2013-08-02 16:44:35 +0800

    Update mona to 1.4-15.

--- mona.rb
+++ mona.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mona < Formula
   homepage 'http://www.brics.dk/mona/'
-  url 'http://www.brics.dk/mona/download/mona-1.4-13.tar.gz'
-  sha1 '1ca3472280d70cc1be82a864d7cff310c3a7a936'
+  url 'http://www.brics.dk/mona/download/mona-1.4-15.tar.gz'
+  sha1 '7261d429299968372051cdd7c98bcf9be3c360dc'
 
   def install
     system "./configure", "--prefix=#{prefix}",
