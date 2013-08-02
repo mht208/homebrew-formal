7955ae90a4e24ea880b279696027abaa45ec32d8
Ming-Hsien Tsai <mhtsai208@gmail.com>
2013-08-02 15:45:59 +0800

    Update alt-ergo to 0.95.1.

--- alt-ergo.rb
+++ alt-ergo.rb
@@ -2,10 +2,8 @@ require 'formula'
 
 class AltErgo < Formula
   homepage 'http://alt-ergo.lri.fr'
-  url 'http://alt-ergo.lri.fr/http/alt-ergo-0.94.tar.gz'
-  sha1 '52d32f5ba9ea1ce7bf90ce204d92571d1f62d6db'
-
-  option 'with-gui', 'Build GUI'
+  url 'http://alt-ergo.lri.fr/http/alt-ergo-0.95.1/alt-ergo-0.95.1.tar.gz'
+  sha1 'eae5cc58bc28bbfd369f5c6ef8f767c835ec9ce9'
 
   depends_on 'objective-caml'
   depends_on 'lablgtk2' if build.include? 'with-gui'
@@ -20,26 +18,24 @@ class AltErgo < Formula
                           "--mandir=#{man}"
     system "make"
     system "make install"
-    if build.include? 'with-gui'
-      system "make gui"
-      mkdir_p "#{share}/gtksourceview-2.0/language-specs"
-      system "make install-gui"
-    end
   end
 end
 
 
 __END__
 diff --git a/Makefile.in b/Makefile.in
-index 04a5b0e..6cbfa2d 100644
+index f7b0f59..a42cae8 100644
 --- a/Makefile.in
 +++ b/Makefile.in
-@@ -208,7 +209,7 @@ install-man:
- install-gui: install-indep
- 	mkdir -p $(BINDIR)
+@@ -245,8 +245,8 @@ install: install-indep
+ 	cp -f $(NAME).$(OCAMLBEST) $(BINDIR)/$(NAME)$(EXE)
+ ifeq ($(ENABLEGUI),yes)
  	cp -f altgr-ergo.opt $(BINDIR)/altgr-ergo$(EXE)
--	cp -f util/gtk-lang/alt-ergo.lang /usr/share/gtksourceview-2.0/language-specs/alt-ergo.lang
+-	mkdir -p $(DATADIR)/gtksourceview-2.0/language-specs
+-	cp -f util/gtk-lang/alt-ergo.lang $(DATADIR)/gtksourceview-2.0/language-specs/alt-ergo.lang
++	mkdir -p @datarootdir@/gtksourceview-2.0/language-specs
 +	cp -f util/gtk-lang/alt-ergo.lang @datarootdir@/gtksourceview-2.0/language-specs/alt-ergo.lang
+ endif
  
- install-pack: xpack pack
- 	mkdir -p $(LIBDIR)
+ install-pack: xpack pack META
+
