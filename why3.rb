86203816112d7cc784a79c4e70d08756fde4fb4e
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-01-16 23:03:43 +0800

    Update the options of why3.

--- why3.rb
+++ why3.rb
@@ -8,9 +8,11 @@ class Why3 < Formula
   option 'without-native', 'Build without the native OCaml compiler'
   option 'with-doc', 'Install documentations'
   option 'with-ide', 'Install IDE'
+  option 'with-coq-libs', 'Enable Coq realizations'
+  option 'with-coq-tactic', 'Enable Coq "why3" tactic'
 
   depends_on 'objective-caml'
-  depends_on 'coq' if build.include? 'with-coq'
+  depends_on 'coq' if build.include? 'with-coq-libs' or build.include? 'with-coq-tactics'
   depends_on 'lablgtk2' if build.include? 'with-ide'
 
   def install
@@ -23,10 +25,11 @@ class Why3 < Formula
             "--enable-hypothesis-selection",
             "--disable-debug",
             "--disable-profiling",
-            "--disable-pvs-libs",
-            "--disable-coq-plugin", "--disable-coq-libs"]
+            "--disable-pvs-libs"]
     args << ((build.include? 'without-native') ? '--disable-native-code' : '--enable-native-code')
     args << ((build.include? 'with-doc') ? '--enable-doc' : '--disable-doc')
+    args << ((build.include? 'with-coq-libs') ? '--enable-coq-libs' : '--disable-coq-libs')
+    args << ((build.include? 'with-coq-tactic') ? '--enable-coq-tactic' : '--disable-coq-tactic')
     system "./configure", *args
 
     # Fix a bug
