d6843f60a5af9219923160129d106e6be87c7285
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-01-20 10:53:10 +0800

    Update gappa to 1.1.0.

--- gappa.rb
+++ gappa.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Gappa < Formula
   homepage 'http://gappa.gforge.inria.fr'
-  url 'https://gforge.inria.fr/frs/download.php/32744/gappa-1.0.0.tar.gz'
-  sha1 'e2d5fd30f4523b124c3ed2cf5219d54d99f27b29'
+  url 'https://gforge.inria.fr/frs/download.php/33305/gappa-1.1.0.tar.gz'
+  sha1 '4752c3243b98edadaa959390e07167c136c79aa9'
 
   depends_on "gmp"
   depends_on "mpfr"
   depends_on "boost"
 
   def install
-    ENV['CXX'] = "g++"
     system "./configure"
     system "./remake"
     bin.install 'src/gappa'
