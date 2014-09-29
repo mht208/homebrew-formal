c602c0d2554ea73fdd237d4afbb37e9db7c06a57
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-09-29 11:18:16 +0800

    Update alt-ergo to 0.95.2.

--- alt-ergo.rb
+++ alt-ergo.rb
@@ -2,40 +2,22 @@ require 'formula'
 
 class AltErgo < Formula
   homepage 'http://alt-ergo.lri.fr'
-  url 'http://alt-ergo.lri.fr/http/alt-ergo-0.95.1/alt-ergo-0.95.1.tar.gz'
-  sha1 'eae5cc58bc28bbfd369f5c6ef8f767c835ec9ce9'
+  url 'http://alt-ergo.ocamlpro.com/http/alt-ergo-0.95.2/alt-ergo-0.95.2.tar.gz'
+  sha1 '6555c40ba9d5690e98093c1ed2b53090e6f35ef2'
+
+  option 'with-gui', 'Install graphical user interface'
 
   depends_on 'objective-caml'
-  depends_on 'lablgtk2' if build.include? 'with-gui'
-
-  def patches
-    DATA
-  end
+  depends_on 'ocamlgraph'
+  depends_on 'lablgtk2' if build.with? 'gui'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--datarootdir=#{share}",
-                          "--mandir=#{man}"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
+    if build.with? 'gui'
+      system "make gui"
+      system "make install-gui"
+    end
   end
 end
-
-
-__END__
-diff --git a/Makefile.in b/Makefile.in
-index f7b0f59..a42cae8 100644
---- a/Makefile.in
-+++ b/Makefile.in
-@@ -245,8 +245,8 @@ install: install-indep
- 	cp -f $(NAME).$(OCAMLBEST) $(BINDIR)/$(NAME)$(EXE)
- ifeq ($(ENABLEGUI),yes)
- 	cp -f altgr-ergo.opt $(BINDIR)/altgr-ergo$(EXE)
--	mkdir -p $(DATADIR)/gtksourceview-2.0/language-specs
--	cp -f util/gtk-lang/alt-ergo.lang $(DATADIR)/gtksourceview-2.0/language-specs/alt-ergo.lang
-+	mkdir -p @datarootdir@/gtksourceview-2.0/language-specs
-+	cp -f util/gtk-lang/alt-ergo.lang @datarootdir@/gtksourceview-2.0/language-specs/alt-ergo.lang
- endif
- 
- install-pack: xpack pack META
-
