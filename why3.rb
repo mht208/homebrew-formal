0f670fe2fb2cbfed534d778e6b7d4cfb7c0d9443
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-09-29 13:08:06 +0800

    Update Why3 to 0.85.

--- why3.rb
+++ why3.rb
@@ -2,18 +2,22 @@ require 'formula'
 
 class Why3 < Formula
   homepage 'http://why3.lri.fr'
-  url 'http://why3.lri.fr/download/why3-0.82.tar.gz'
-  sha1 '40a08a204e3d947ac46253121b9f6ed7ff3efdfa'
+  url 'https://gforge.inria.fr/frs/download.php/file/34074/why3-0.85.tar.gz'
+  sha1 'c8d5f56c80f936a667ea2719baa99bb84a5f05ca'
 
   option 'without-native', 'Build without the native OCaml compiler'
   option 'with-doc', 'Install documentations'
   option 'with-ide', 'Install IDE'
   option 'with-coq-libs', 'Enable Coq realizations'
   option 'with-coq-tactic', 'Enable Coq "why3" tactic'
+  option 'with-profiling', 'Enable profiling'
+  option 'with-flocq', 'Enable floating point arithmetic'
+  option 'with-gmp', 'Enable GMP arithmetic'
 
   depends_on 'objective-caml'
-  depends_on 'coq' if build.include? 'with-coq-libs' or build.include? 'with-coq-tactics'
-  depends_on 'lablgtk2' => 'with-gtksourceview2' if build.include? 'with-ide'
+  depends_on 'coq' if build.with? 'coq-libs' or build.with? 'coq-tactics'
+  depends_on 'lablgtk2' => 'with-gtksourceview2' if build.with? 'ide'
+  depends_on 'gmp' if build.with? 'gmp' => :optional
 
   def install
     homebrew_prefix_stdlib = `ocamlc -where`.gsub /\n/, ""
@@ -21,22 +25,15 @@ class Why3 < Formula
 
     # The current version does not support Coq-8.4 and PVS
     args = ["--prefix=#{prefix}",
-            "--disable-bench",
             "--enable-hypothesis-selection",
             "--disable-debug",
-            "--disable-profiling",
             "--disable-pvs-libs"]
-    args << ((build.include? 'without-native') ? '--disable-native-code' : '--enable-native-code')
-    args << ((build.include? 'with-doc') ? '--enable-doc' : '--disable-doc')
-    args << ((build.include? 'with-coq-libs') ? '--enable-coq-libs' : '--disable-coq-libs')
-    args << ((build.include? 'with-coq-tactic') ? '--enable-coq-tactic' : '--disable-coq-tactic')
+    args << ((build.without? 'native') ? '--disable-native-code' : '--enable-native-code')
+    args << ((build.with? 'doc') ? '--enable-doc' : '--disable-doc')
+    args << ((build.with? 'coq-libs') ? '--enable-coq-libs' : '--disable-coq-libs')
+    args << ((build.with? 'coq-tactic') ? '--enable-coq-tactic' : '--disable-coq-tactic')
+    args << ((build.with? 'profiling') ? '--enable-profiling' : '--disable-profiling')
     system "./configure", *args
-
-    # Fix a bug
-    if build.include? 'without-native'
-      chmod 0644, 'Makefile'
-      inreplace 'Makefile', /OCAMLOPT *= no/, 'OCAMLOPT = true'
-    end
 
     system "make"
     system "make byte"
