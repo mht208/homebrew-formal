b092f02a0df5dfd0058705472938b3e91816846c
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-09-29 17:37:11 +0800

    Update z3.

--- z3.rb
+++ z3.rb
@@ -6,31 +6,30 @@ class Z3 < Formula
   sha1 '80d68d08e15ec40ba314c226f065b2def550fca4'
 
   depends_on 'autoconf' => :build
-  depends_on :python
+  depends_on 'python'
 
   def install
-    # Although /usr/bin/c++ is a symbolic link to /usr/bin/clang++, it seems that Z3
-    # does not like the name c++.
-    ENV['CXX'] = "clang++"
+    pyver = `python --version 2>&1 | awk '{print $2}'`.chomp.gsub(/([0-9]+).([0-9]+).([0-9]+)/, '\1.\2')
+    pylocal = lib.to_s << "/python#{pyver}/site-packages"
+    pyglobal = "/usr/local/lib/python#{pyver}/site-packages"
 
-    inreplace "scripts/mk_util.py", "distutils.sysconfig.get_python_lib()", "'#{python.site_packages}'"
+    inreplace "scripts/mk_util.py", "distutils.sysconfig.get_python_lib()", "'#{pylocal}'"
     system "autoconf"
     system "./configure --prefix=#{prefix}"
-    system python, "scripts/mk_make.py"
-    mkdir_p "#{python.site_packages}"
+    system "python scripts/mk_make.py"
+    mkdir_p "#{pylocal}"
+
     Dir.chdir "build" do
       system "make"
       system "make PREFIX=#{prefix} install"
     end
 
     ohai "Linking python bindings"
-    Dir["#{python.site_packages}/*.pyc"].each do |f|
-      path = python.global_site_packages/(Pathname.new(f).basename)
+    Dir["#{pylocal}/*.pyc"].each do |f|
+      path = Pathname.new(pyglobal) + Pathname.new(f).basename
       puts path
       rm path if path.exist?
       ln_s f, path
     end
-    puts "#{python.global_site_packages}/libz3.dylib"
-    ln_s "#{python.site_packages}/libz3.dylib", "#{python.global_site_packages}/libz3.dylib"
   end
 end
