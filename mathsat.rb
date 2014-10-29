628772476dc1ccdd9dcdec34d4730b77358c9e5a
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-10-29 08:54:23 +0800

    Update mathsat to 5.2.12.

--- mathsat.rb
+++ mathsat.rb
@@ -2,14 +2,28 @@ require 'formula'
 
 class Mathsat < Formula
   homepage 'http://mathsat.fbk.eu/index.html'
-  url 'http://mathsat.fbk.eu/download.php?file=mathsat-5.2.8-darwin-libcxx-x86_64.tar.gz'
-  version '5.2.8'
-  sha1 'b89776c4648c0d4f8a503c4757e15dfa0964f6f6'
+  url 'http://mathsat.fbk.eu/download.php?file=mathsat-5.2.12-darwin-libcxx-x86_64.tar.gz'
+  version '5.2.12'
+  sha1 '7ebc3f28b4590b05885520b30b98bd3fbe046029'
+
+  depends_on :python
+  depends_on 'gmp'
 
   def install
+    pyver = `python --version 2>&1 | awk '{print $2}'`.chomp.gsub(/([0-9]+).([0-9]+).([0-9]+)/, '\1.\2')
+    pylocal = (lib/"python#{pyver}/site-packages")
+
+    # Compile python bindings.
+    Dir.chdir 'python' do
+      system "python setup.py build"
+    end
+
+    # Install.
     bin.install 'bin/mathsat'
     include.install 'include/mathsat.h'
     lib.install 'lib/libmathsat.a'
     (share/'mathsat').install 'configurations', 'examples'
+    mkdir_p "#{pylocal}"
+    pylocal.install "python/mathsat.py", Dir["python/build/lib*/_mathsat.so"]
   end
 end
