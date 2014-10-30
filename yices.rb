67dacd973dbebec538efad141e2ffe245530878f
Ming-Hsien Tsai <mhtsai208@gmail.com>
2014-10-30 13:30:21 +0800

    Update yices to 2.2.2.

--- yices.rb
+++ yices.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class YicesDownloadStrategy < CurlDownloadStrategy
   def _fetch
-    if not File.exists? "#{HOMEBREW_CACHE}/yices-1.0.39-x86_64-apple-darwin10.8.0-static-gmp.tar.gz"
+    if not File.exists? "#{HOMEBREW_CACHE}/yices-2.2.2-x86_64-apple-darwin13.3.0-static-gmp.tar.gz"
       onoe <<-EOS.undent
       Due to license restrictions, we cannot fetch the distributables
       automagically.
 
-      1. Visit http://yices.csl.sri.com/cgi-bin/yices-newlicense.cgi?file=yices-1.0.39-x86_64-apple-darwin10.8.0-static-gmp.tar.gz and click 'I accept'.
+      1. Visit http://yices.csl.sri.com/cgi-bin/yices2-newnewlicense.cgi?file=yices-2.2.2-x86_64-apple-darwin13.3.0-static-gmp.tar.gz and click 'I accept'.
       2. Move file to #{HOMEBREW_CACHE}
-      3. Rename the tarball to yices-1.0.39.tar.gz.
+      3. Rename the tarball to yices-2.2.2.tar.gz.
       4. Install this package again.
       EOS
     end
@@ -18,14 +18,12 @@ end
 
 class Yices < Formula
   homepage 'http://yices.csl.sri.com'
-  url 'http://yices.csl.sri.com/yices-1.0.39-x86_64-apple-darwin10.8.0-static-gmp.tar.gz',
+  url 'http://yices.csl.sri.com/yices-2.2.2-x86_64-apple-darwin13.3.0-static-gmp.tar.gz',
       :using => YicesDownloadStrategy
-  sha1 'd2ae7000294869d1007eea3ff7c6652ed1cbcbdc'
+  sha1 'c94e4010bbcee9a8a6afceb3969226ce21c684de'
 
   def install
-    bin.install "bin/yices"
-    lib.install "lib/libyices.a"
-    include.install "include/yices_c.h", "include/yicesl_c.h"
+    system "./install-yices #{prefix}"
   end
 
 end
