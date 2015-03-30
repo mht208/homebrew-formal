c9913cfea52e9034ae2fdeb7c9417f5ba037a2a2
Ming-Hsien Tsai <mhtsai208@gmail.com>
2015-03-30 12:10:19 +0800

    Update z3 to 4.3.2.

--- z3.rb
+++ z3.rb
@@ -2,34 +2,19 @@ require 'formula'
 
 class Z3 < Formula
   homepage 'http://z3.codeplex.com'
-  url 'https://git01.codeplex.com/z3', :tag => 'v4.3.1', :using => :git
-  sha1 '80d68d08e15ec40ba314c226f065b2def550fca4'
+  url 'https://git.codeplex.com/z3', :using => :git, :tag => 'v4.3.2', :revision => 'cee7dd39444c9060186df79c2a2c7f8845de415b'
+  sha1 'cbfbec39c1c671a932bfd86ff34aebd0f5a4b7aa'
 
   depends_on 'autoconf' => :build
-  depends_on 'python'
+  depends_on :'python'
 
   def install
-    pyver = `python --version 2>&1 | awk '{print $2}'`.chomp.gsub(/([0-9]+).([0-9]+).([0-9]+)/, '\1.\2')
-    pylocal = lib.to_s << "/python#{pyver}/site-packages"
-    pyglobal = "/usr/local/lib/python#{pyver}/site-packages"
-
-    inreplace "scripts/mk_util.py", "distutils.sysconfig.get_python_lib()", "'#{pylocal}'"
-    system "autoconf"
-    system "./configure --prefix=#{prefix}"
-    system "python scripts/mk_make.py"
-    mkdir_p "#{pylocal}"
+    inreplace "scripts/mk_util.py", "dist-packages", "site-packages"
+    system "python scripts/mk_make.py --prefix=#{prefix}"
 
     Dir.chdir "build" do
       system "make"
       system "make PREFIX=#{prefix} install"
-    end
-
-    ohai "Linking python bindings"
-    Dir["#{pylocal}/*.pyc"].each do |f|
-      path = Pathname.new(pyglobal) + Pathname.new(f).basename
-      puts path
-      rm path if path.exist?
-      ln_s f, path
     end
   end
 end
