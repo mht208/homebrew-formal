a2ce9f8511267baf1c164951e5cae98560c25bf5
Ming-Hsien Tsai <mhtsai208@gmail.com>
2015-06-30 10:47:11 +0800

    Update cbmc to 5.1.

--- cbmc.rb
+++ cbmc.rb
@@ -2,19 +2,14 @@ require 'formula'
 
 class Cbmc < Formula
   homepage 'http://www.cprover.org/cbmc/'
-  url 'http://www.cprover.org/svn/cbmc/releases/cbmc-4.7', :using => :svn
+  url 'http://www.cprover.org/svn/cbmc/releases/cbmc-5.1', :using => :svn
 
   def install
     Dir.chdir 'src' do
       system "make minisat2-download"
-      inreplace '../minisat-2.2.0/core/SolverTypes.h',
-                'mkLit(Var var, bool sign = false);',
-                'mkLit(Var var, bool sign);'
-      inreplace '../minisat-2.2.0/core/SolverTypes.h',
-                'inline  Lit  mkLit',
-                "Lit mkLit(Var var, bool sign = false);\ninline  Lit  mkLit"
       system "make"
-      bin.install "cbmc/cbmc"
+      bin.install "cbmc/cbmc", "goto-cc/goto-cc", 
+                  "goto-instrument/goto-instrument"
     end
   end
 end
