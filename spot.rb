4922e7b90c3658cde6ea5add986af376a505912b
Ming-Hsien Tsai <mhtsai208@gmail.com>
2015-10-16 13:46:14 +0800

    Update Spot to 1.99.4.

--- spot.rb
+++ spot.rb
@@ -1,16 +1,19 @@
 require 'formula'
 
 class Spot < Formula
-  homepage 'http://spot.lip6.fr/'
-  url 'http://spot.lip6.fr/dl/spot-1.1.4.tar.gz'
-  sha1 'fb026fe131fdec1bd486a12da540414591a1f190'
+  homepage 'https://spot.lrde.epita.fr/index.html'
+  url 'http://www.lrde.epita.fr/dload/spot/spot-1.99.4.tar.gz'
+  sha256 '89ab540ecf8a12045f8fa6cbeb6995eaaab213d815c4baee108d3744380bff09'
 
   def install
     # Since Spot uses modified Buddy and Lbtt, we build statically linked Spot.
-    system "./configure", "--prefix=#{prefix}", "--disable-shared"
+    system "./configure", "--prefix=#{prefix}", "--disable-shared", "--disable-python"
     system "make"
-    bin.install "src/bin/genltl", "src/bin/ltl2tgba", "src/bin/ltl2tgta", "src/bin/ltlcross", "src/bin/ltlfilt", "src/bin/randltl"
-    man1.install "src/bin/man/genltl.1", "src/bin/man/ltl2tgba.1", "src/bin/man/ltl2tgta.1", "src/bin/man/ltlcross.1", "src/bin/man/ltlfilt.1", "src/bin/man/randltl.1"
+    bin.install "src/bin/autfilt", "src/bin/dstar2tgba", "src/bin/genltl",
+                "src/bin/ltl2tgba", "src/bin/ltl2tgta", "src/bin/ltlcross",
+                "src/bin/ltldo", "src/bin/ltlfilt", "src/bin/ltlgrind",
+                "src/bin/randaut", "src/bin/randltl", "src/bin/spot-x"
+    man1.install Dir["src/bin/man/*.1"], Dir["src/bin/man/*.x"]
     doc.install "doc/tl/tl.pdf"
     lib.install "src/.libs/libspot.a", "src/.libs/libspot.la", "src/.libs/libspot.lai"
   end
