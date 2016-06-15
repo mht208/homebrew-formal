1e32572da67167bd4be35fd0ad5707199a294c03
Ming-Hsien Tsai <mhtsai208@gmail.com>
2016-06-15 12:04:54 +0800

    Add csdp.

---
+++ csdp.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Csdp < Formula
+  desc 'CSDP is a library of routines that implements a predictor corrector variant of the semidefinite programming algorithm of Helmberg, Rendl, Vanderbei, and Wolkowicz'
+  homepage 'https://projects.coin-or.org/Csdp'
+  url 'https://github.com/coin-or/Csdp/archive/releases/6.1.1.tar.gz'
+  sha256 'd4d67dc289e49d113af85fab1ba8dd6929f4d7d53d8234e831498e6642b63a96'
+
+  depends_on 'gcc'
+
+  fails_with :llvm do
+  end
+
+  fails_with :clang do
+  end
+
+  def install
+    system "make"
+    bin.install "solver/csdp", "theta/theta", "theta/graphtoprob",
+                "theta/complement", "theta/rand_graph"
+    lib.install "lib/libsdp.a"
+    (include/"csdp").install Dir["include/*.h"]
+  end
+end
