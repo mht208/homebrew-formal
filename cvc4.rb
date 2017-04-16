252e7450cf14ce066ffb66117b846c2381fccd20
Michael Belyaev <usr-sse2@users.noreply.github.com>
2017-04-16 22:24:03 +0300

    Update cvc4 download path
    The old one is no more valid

--- cvc4.rb
+++ cvc4.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Cvc4 < Formula
   homepage 'http://cvc4.cs.nyu.edu'
-  url 'http://cvc4.cs.nyu.edu/builds/macos/ports/cvc4/cvc4-1.4_2.darwin_13.x86_64.tbz2'
+  url 'http://cvc4.cs.stanford.edu/downloads/builds/macos/ports/cvc4/cvc4-1.4_2.darwin_13.x86_64.tbz2'
   version '1.4.2'
   sha256 '716da2dc4e16e139b64dad5b417793c1632f23dcfcba42194cc859e5ad9145fe'
 
