482fe6e463ee2779ac4a92b4b9cb8b6a6c648993
Ming-Hsien Tsai <mhtsai208@gmail.com>
2018-05-07 17:10:59 +0800

    Update cudd.

--- cudd.rb
+++ cudd.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Cudd < Formula
+  desc 'CUDD is a package written in C for the manipulation of decision diagrams. Note that the official website of CUDD is down. This formula will install CUDD from an unofficial mirror on GitHub.'
   homepage 'http://vlsi.colorado.edu/~fabio/CUDD/'
-  url 'ftp://vlsi.colorado.edu/pub/cudd-3.0.0.tar.gz'
-  sha256 'b8e966b4562c96a03e7fbea239729587d7b395d53cadcc39a7203b49cf7eeb69'
+  url 'https://github.com/ivmai/cudd/archive/cudd-3.0.0.tar.gz'
+  sha256 '5fe145041c594689e6e7cf4cd623d5f2b7c36261708be8c9a72aed72cf67acce'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--enable-shared", "--enable-dddmp", "--enable-obj"
