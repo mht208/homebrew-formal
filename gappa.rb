479a69ed3df59f5d45ca5922667fc9f0ad768825
Ming-Hsien Tsai <mhtsai208@gmail.com>
2018-12-22 09:50:24 +0800

    Update spot.

--- gappa.rb
+++ gappa.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Gappa < Formula
   homepage 'http://gappa.gforge.inria.fr'
   url 'https://gforge.inria.fr/frs/download.php/file/37624/gappa-1.3.3.tar.gz'
-  sha256 '597f58d38ffb0c025352763a387425242723045f398ab2ad65d6f5f61b13681a'
+  sha256 '1c88d2ae96cd88a49af37393d06e92ffa20abdc8b170830fcdcde03ae8208e44'
 
   depends_on "gmp"
   depends_on "mpfr"
--- spot.rb
+++ spot.rb
@@ -2,19 +2,14 @@ require 'formula'
 
 class Spot < Formula
   homepage 'https://spot.lrde.epita.fr/index.html'
-  url 'http://www.lrde.epita.fr/dload/spot/spot-1.99.4.tar.gz'
-  sha256 '89ab540ecf8a12045f8fa6cbeb6995eaaab213d815c4baee108d3744380bff09'
+  url 'http://www.lrde.epita.fr/dload/spot/spot-2.7.tar.gz'
+  sha256 '3ea000ac443c780f09574ec059c8931436783227c6afb43b24c863e85a22b5ef'
+
+  depends_on 'python3'
 
   def install
-    # Since Spot uses modified Buddy and Lbtt, we build statically linked Spot.
-    system "./configure", "--prefix=#{prefix}", "--disable-shared", "--disable-python"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
-    bin.install "src/bin/autfilt", "src/bin/dstar2tgba", "src/bin/genltl",
-                "src/bin/ltl2tgba", "src/bin/ltl2tgta", "src/bin/ltlcross",
-                "src/bin/ltldo", "src/bin/ltlfilt", "src/bin/ltlgrind",
-                "src/bin/randaut", "src/bin/randltl", "src/bin/spot-x"
-    man1.install Dir["src/bin/man/*.1"], Dir["src/bin/man/*.x"]
-    doc.install "doc/tl/tl.pdf"
-    lib.install "src/.libs/libspot.a", "src/.libs/libspot.la", "src/.libs/libspot.lai"
+    system "make", "install"
   end
 end
