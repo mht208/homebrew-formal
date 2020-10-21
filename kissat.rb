c60daea1d398756c81e5d1e25fe5639081086454
Ming-Hsien Tsai <mhtsai208@gmail.com>
2020-10-21 10:56:07 +0800

    Add kissat.

---
+++ kissat.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Kissat < Formula
+  desc 'a condensed and improved reimplementation of CaDiCaL in C'
+  homepage 'http://fmv.jku.at/kissat/'
+  url 'https://github.com/arminbiere/kissat/archive/sc2020.tar.gz'
+  sha256 'a1adb4f2be535bec9126c451b2b49e0142997ba0b2ac79280a48aa575b4426dd'
+  version 'sc2020'
+
+  def install
+    system './configure'
+    system 'make'
+    bin.install 'build/kissat'
+  end
+
+end
