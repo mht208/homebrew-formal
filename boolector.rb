27e35d526a238925ac7678a0f3cd0411a02eb7c5
Ming-Hsien Tsai <mhtsai208@gmail.com>
2020-12-11 20:49:33 +0800

    Update boolector to 3.2.0.

--- boolector.rb
+++ boolector.rb
@@ -1,84 +1,77 @@
 require 'formula'
 
 class Boolector < Formula
+  include Language::Python::Virtualenv
+
   desc 'an SMT solver'
   homepage 'http://fmv.jku.at/boolector/'
-  url 'https://github.com/Boolector/boolector/archive/3.0.0.tar.gz'
-  sha256 '1db846f73643059a8bf533efd6c306c7d20c161dfe185b07923dc35b1f35a513'
-
-  option 'with-doc', 'Install documentation'
-  option 'without-python', 'Do not install python API (because it seems not working)'
+  url 'https://github.com/Boolector/boolector/archive/3.2.0.tar.gz'
+  sha256 '5065ed4032f2761aff509a99df2124c0de1ab4fc8e7407d271946c92d564f268'
 
   depends_on 'cmake'
-  depends_on 'sphinx-doc' if build.with? 'doc'
+  depends_on 'python@3.9'
+  depends_on 'gmp'
+  depends_on 'sphinx-doc'
 
   patch :DATA
 
+  resource "Cython" do
+    url "https://files.pythonhosted.org/packages/6c/9f/f501ba9d178aeb1f5bf7da1ad5619b207c90ac235d9859961c11829d0160/Cython-0.29.21.tar.gz"
+    sha256 "e57acb89bd55943c8d8bf813763d20b9099cc7165c0f16b707631a7654be9cad"
+  end
+
   def install
+    venv_root = libexec/"venv"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", "#{venv_root}/lib/python#{xy}/site-packages"
+    venv = virtualenv_create(venv_root, "python3")
+    venv.pip_install resource("Cython")
+
     # Build dependencies
     system "./contrib/setup-btor2tools.sh"
     system "./contrib/setup-cadical.sh"
     system "./contrib/setup-lingeling.sh"
-    system "./contrib/setup-minisat.sh"
-    #system "./contrib/setup-picosat.sh" # Failed to compile
 
-    args = []
-    args << ((build.with? 'python') ? '--python' : '')
+    args = ["--prefix", "#{prefix}", "--python", "--py3", "--gmp"]
 
     # Configure Boolector
     system "./configure.sh", *args
 
     # Compile Boolector
     Dir.chdir 'build' do
-      if build.with? 'python' then
-        Dir.chdir 'src/api/python' do
-          system "make pyboolector_options"
-        end
-      end
       system "make"
     end
 
-    if build.with? 'doc' then
-      Dir.chdir 'doc' do
-        system "make html"
-        system "make man"
-      end
+    # Compile documentation
+    Dir.chdir 'doc' do
+      system "make", "html"
+      system "make", "man"
     end
 
     # Install
     Dir.chdir 'build' do
-      bin.install "bin/boolector", "bin/btorimc", "bin/btormbt",
-                  "bin/btormc", "bin/btoruntrace"
-      lib.install "lib/libboolector.a"
-      lib.install "lib/pyboolector.so" if build.with? 'python'
+      system "make", "install"
     end
-    Dir.chdir 'src' do
-      (include/'boolector').install Dir['*.h']
-      (include/'boolector'/'dumper').install Dir['dumper/*.h']
-      (include/'boolector'/'parser').install Dir['parser/*.h']
-      (include/'boolector'/'simplifier').install Dir['simplifier/*.h']
-      (include/'boolector'/'utils').install Dir['utils/*.h']
-    end
-    if build.with? 'doc' then
-      (share/'boolector').install 'doc/_build/html'
-      man1.install 'doc/_build/man/boolector.1'
-    end
+
+    (share/'boolector').install 'doc/_build/html'
+    man1.install 'doc/_build/man/boolector.1'
     (share/'boolector').install 'examples'
   end
 
 end
 
 __END__
-diff --git a/contrib/setup-minisat.sh b/contrib/setup-minisat.sh
-index 61a0c64..8f19462 100755
---- a/contrib/setup-minisat.sh
-+++ b/contrib/setup-minisat.sh
-@@ -10,6 +10,6 @@ mkdir -p ${SETUP_DIR}
- MINISAT_DIR=${SETUP_DIR}/minisat
+diff --git a/contrib/setup-cadical.sh b/contrib/setup-cadical.sh
+index ddddc9c..006dca5 100755
+--- a/contrib/setup-cadical.sh
++++ b/contrib/setup-cadical.sh
+@@ -21,7 +21,7 @@ if is_windows; then
+   #
+   export CXXFLAGS=""
+ else
+-  export CXXFLAGS="-fPIC"
++  export CXXFLAGS="-fPIC -std=c++0x"
+ fi
  
- # Download and build MiniSat
--git clone --depth 1 https://github.com/niklasso/minisat.git ${MINISAT_DIR}
-+git clone --depth 1 https://github.com/mht208/minisat.git ${MINISAT_DIR}
- cd ${MINISAT_DIR}
- make -j${NPROC}
+ ./configure ${EXTRA_FLAGS}
 
