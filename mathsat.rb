cefb7e00f077fb97fa7ddeb25cb7438428b9ee38
Ming-Hsien Tsai <mhtsai208@gmail.com>
2022-03-22 10:14:00 +0800

    Update mathsat 5.6.6.

--- mathsat.rb
+++ mathsat.rb
@@ -2,19 +2,20 @@ require 'formula'
 
 class Mathsat < Formula
   homepage 'http://mathsat.fbk.eu/index.html'
-  url 'https://mathsat.fbk.eu/release/mathsat-5.6.5-darwin-libcxx-x86_64.tar.gz'
-  version '5.6.5'
-  sha256 'bf13877df67e1a9529474644de2999ba3b5e314f82a8790138e4ddd5c5cd5bfa'
+  url 'https://mathsat.fbk.eu/release/mathsat-5.6.6-darwin-libcxx-x86_64.tar.gz'
+  version '5.6.6'
+  sha256 'e87e42778b701d76935c216faa0233a6654dbd50ec9a03c1f7ca074d4e1c65af'
 
   depends_on 'gmp'
+  depends_on 'python'
 
   def install
-    pyver = `python --version 2>&1 | awk '{print $2}'`.chomp.gsub(/([0-9]+).([0-9]+).([0-9]+)/, '\1.\2')
+    pyver = `python3 --version 2>&1 | awk '{print $2}'`.chomp.gsub(/([0-9]+).([0-9]+).([0-9]+)/, '\1.\2')
     pylocal = (lib/"python#{pyver}/site-packages")
 
     # Compile python bindings.
     Dir.chdir 'python' do
-      system "python setup.py build"
+      system "python3 setup.py build"
     end
 
     # Install.
@@ -23,6 +24,6 @@ class Mathsat < Formula
     lib.install 'lib/libmathsat.a', 'lib/libmathsat.dylib'
     (share/'mathsat').install 'configurations', 'examples'
     mkdir_p "#{pylocal}"
-    pylocal.install "python/mathsat.py", Dir["python/build/lib*/_mathsat.so"]
+    pylocal.install "python/mathsat.py", Dir["python/build/lib*/_mathsat*.so"]
   end
 end
