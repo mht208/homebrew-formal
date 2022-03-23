282b3366bd67c3142ae1adc90a2a5d091b38b817
Ming-Hsien Tsai <mhtsai208@gmail.com>
2022-03-23 10:51:18 +0800

    Update cvc5 to 0.0.7.

--- cvc5.rb
+++ cvc5.rb
@@ -1,25 +1,13 @@
 require 'formula'
 
 class Cvc5 < Formula
-  include Language::Python::Virtualenv
-
   homepage 'https://cvc5.github.io'
-  url 'https://github.com/cvc5/cvc5/archive/refs/tags/cvc5-0.0.4.tar.gz'
-  sha256 '8c7eb6d4b372917f82c7a0889ae4ace05229e82e8afd31d1a9c79ed7db7d079b'
-
-  depends_on "coreutils" => :build
-  depends_on "cmake" => :build
-  depends_on "python@3.9"
-  depends_on "gmp" => :build
-  depends_on "cadical"
+  desc 'CVC5 is an efficient open-source automatic theorem prover for satisfiability modulo theories (SMT) problems.'
+  url 'https://github.com/cvc5/cvc5/releases/latest/download/cvc5-macOS'
+  sha256 '224f3562eb1af44b13d7d6a284d1136efcdf32d0636274703654c95fbc035cdf'
+  version '0.0.7'
 
   def install
-    args = [ "--prefix=#{prefix}",
-             "--auto-download" ]
-    system "./configure.sh", *args
-    chdir "build" do
-      system "make"
-      system "make", "install"
-    end
+    bin.install "cvc5-macOS" => "cvc5"
   end
 end
