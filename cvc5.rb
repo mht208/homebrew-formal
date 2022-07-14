460fb901f5d8fe6532dc734760a5dfe2ef09b5ff
Ming-Hsien Tsai <mhtsai208@gmail.com>
2022-07-14 08:59:51 +0800

    Update cvc5 to 1.0.0.

--- cvc5.rb
+++ cvc5.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Cvc5 < Formula
   homepage 'https://cvc5.github.io'
   desc 'CVC5 is an efficient open-source automatic theorem prover for satisfiability modulo theories (SMT) problems.'
-  url 'https://github.com/cvc5/cvc5/releases/latest/download/cvc5-macOS'
-  sha256 '224f3562eb1af44b13d7d6a284d1136efcdf32d0636274703654c95fbc035cdf'
-  version '0.0.7'
+  url 'https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.0/cvc5-macOS'
+  sha256 '43420009f1b305f74d3c92aaf64ff3ce39e85e8601d4b0dc6c7dc33fe7c5be6d'
+  version '1.0.0'
 
   def install
     bin.install "cvc5-macOS" => "cvc5"
