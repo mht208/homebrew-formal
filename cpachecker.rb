9caa0002dd372464ec408724320012b1d0a24310
Luca Di Stefano <lucad@chalmers.se>
2022-11-30 15:18:27 +0100

    Fix cpachecker recipe

--- cpachecker.rb
+++ cpachecker.rb
@@ -3,20 +3,17 @@ require 'formula'
 class Cpachecker < Formula
   desc 'CPAchecker is a tool for configurable software verification.'
   homepage 'http://cpachecker.sosy-lab.org'
-  url 'https://svn.sosy-lab.org/software/cpachecker/tags/cpachecker-2.2/'
-
-  depends_on 'ant'
+  url 'https://cpachecker.sosy-lab.org/CPAchecker-2.2-unix.zip'
+  sha256 'https://cpachecker.sosy-lab.org/CPAchecker-2.2-unix.zip'
 
   def install
-    inreplace 'build/build-ivy.xml', 'https', 'http'
     File.open("cpachecker", "w") { |f|
       f.puts("#!/bin/sh\n")
       f.puts((share/'cpachecker').to_s << "/scripts/cpa.sh \"$@\"")
     }
     File.chmod(0755, "cpachecker")
-    system "ant jar"
     bin.install "cpachecker"
-    (share/'cpachecker').install "Copyright.txt", "LICENSE",
+    (share/'cpachecker').install "Authors.txt", "LICENSE",
                                  "NEWS.md", "README.md", "config",
                                  "cpachecker.jar", "doc", "lib",
                                  "scripts"
