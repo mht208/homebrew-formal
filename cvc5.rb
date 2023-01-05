39f0d46d2ba1c37b12d8e68ade0e6159583dbc92
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-01-05 12:00:16 +0800

    Update cvc5 to 1.0.3.

--- cvc5.rb
+++ cvc5.rb
@@ -1,16 +1,16 @@
 class Cvc5 < Formula
   desc "Efficient open-source automatic theorem prover for SMT problems"
   homepage "https://cvc5.github.io"
-  version "1.0.1"
+  version "1.0.3"
 
   # frozen_string_literal: true
   #
   if Hardware::CPU.intel?
-    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.1/cvc5-macOS"
-    sha256 "67c32310226177ec36d60d377f31eea211c2543bd6487b5c39c39bf9547df568"
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/cvc5-macOS"
+    sha256 "dca671880681ab51e544eab015347f7568174418bb58a22cebe3cefc007120ac"
   else
-    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.1/cvc5-macOS-arm64"
-    sha256 "89b9df4307ff6401c3487b1df166ea77eb7f91b8fd91d52376a1b7660f5930ba"
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/cvc5-macOS-arm64"
+    sha256 "00b29781288f4fca797d0fda3edc887cbf8b07f857d0e45ef567b0e512002336"
   end
 
   def install
