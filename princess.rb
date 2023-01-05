449b0774df6632c8cce484533846b4e1bbcd2e03
Ming-Hsien Tsai <mhtsai208@gmail.com>
2023-01-05 16:58:21 +0800

    Update princess to 2022-11-03.

--- princess.rb
+++ princess.rb
@@ -2,97 +2,42 @@ require 'formula'
 
 class Princess < Formula
   homepage 'http://www.philipp.ruemmer.org/princess.shtml'
-  url 'http://www.philipp.ruemmer.org/princess/princess-2014-08-27.tar.gz'
-  sha256 '8821aa85607a20ad82337662569e979698bd3a0b2aede50593d668c8389fb249'
+  url 'https://github.com/uuverifiers/princess/archive/refs/tags/snapshot-2022-11-03.tar.gz'
+  sha256 '46ae2204f7d81b2954e8678400213d6474bb7bb793a2f7e27059424f1426ac22'
+  version '2022-11-03'
 
   depends_on 'scala'
+  depends_on 'sbt'
 
   patch :DATA
 
   def install
-    system "curl", "http://www2.cs.tum.edu/projects/cup/java-cup-11a.jar", "-o", "extlibs/java-cup-11a.jar"
-    system "make"
+    system "sbt assembly"
 
-    bins = ["princessBench", "princess", "princessGui", "werePrincess"]
+    bins = ["princess", "princessClient", "princessGui"]
     (share/"princess").install bins
     bin.mkdir
     bins.each do |file|
       ln_s share+"princess"+file, bin+file
     end
-    (share/"princess").install "bin"
     (share/"princess"/"parser").install "parser/parser.jar"
     (share/"princess"/"smt-parser").install "smt-parser/smt-parser.jar"
     (share/"princess").install "extlibs"
     (share/"princess").install "wolverine_resources"
+    jars = Dir["target/scala-*/Princess-assembly-*.jar"]
+    jars.each do |jar|
+      p = jar.sub(/\/Princess-assembly.+.jar/, "")
+      (share/"princess"/p).install jar
+    end
   end
 end
 
 __END__
 diff --git a/princess b/princess
-index 134b22a..a9a2f29 100755
+index b9ddc9c..072bca6 100755
 --- a/princess
 +++ b/princess
-@@ -1,6 +1,7 @@
- #!/bin/sh
- 
--BASEDIR=`dirname $(readlink -e $0)`
-+BASEDIR=`dirname $0`/`readlink $0`
-+BASEDIR=`dirname $(readlink $BASEDIR)`
- EXTLIBSDIR=$BASEDIR/extlibs
- 
- export CLASSPATH=$CLASSPATH:$BASEDIR/bin:$BASEDIR/smt-parser/smt-parser.jar:$BASEDIR/parser/parser.jar:$EXTLIBSDIR/java-cup-11a.jar
-diff --git a/princessBench b/princessBench
-index 0a906b9..ded9155 100755
---- a/princessBench
-+++ b/princessBench
-@@ -1,6 +1,7 @@
- #!/bin/sh
- 
--BASEDIR=`dirname $0`
-+BASEDIR=`dirname $0`/`readlink $0`
-+BASEDIR=`dirname $(readlink $BASEDIR)`
- EXTLIBSDIR=$BASEDIR/extlibs
- 
- export CLASSPATH=$CLASSPATH:$BASEDIR/bin:$BASEDIR/parser/parser.jar:$EXTLIBSDIR/java-cup-11a.jar
-diff --git a/princessGui b/princessGui
-index 969ba9b..f086ebd 100755
---- a/princessGui
-+++ b/princessGui
-@@ -1,6 +1,7 @@
- #!/bin/sh
- 
--BASEDIR=`dirname $(readlink -e $0)`
-+BASEDIR=`dirname $0`/`readlink $0`
-+BASEDIR=`dirname $(readlink $BASEDIR)`
- EXTLIBSDIR=$BASEDIR/extlibs
- 
- export CLASSPATH=$CLASSPATH:$BASEDIR/bin:$BASEDIR/parser/parser.jar:$BASEDIR/smt-parser/smt-parser.jar:$EXTLIBSDIR/java-cup-11a.jar
-diff --git a/runTests b/runTests
-index 18fb9c6..3878627 100755
---- a/runTests
-+++ b/runTests
-@@ -2,7 +2,8 @@
- 
- echo -n "Running unit tests "
- 
--BASEDIR=`dirname $0`
-+BASEDIR=`dirname $0`/`readlink $0`
-+BASEDIR=`dirname $(readlink $BASEDIR)`
- EXTLIBSDIR=$BASEDIR/extlibs
- 
- export CLASSPATH=$CLASSPATH:$BASEDIR/bin:$BASEDIR/parser/parser.jar:$EXTLIBSDIR/java-cup-11a.jar
-@@ -13,4 +14,4 @@ scala ap.AllTests "$@"
- echo "Running regression tests"
- 
- cd $BASEDIR/testcases
--./runalldirs
-\ No newline at end of file
-+./runalldirs
-diff --git a/werePrincess b/werePrincess
-index 1d3ccf0..4b6e906 100755
---- a/werePrincess
-+++ b/werePrincess
-@@ -1,14 +1,7 @@
+@@ -1,14 +1,6 @@
  #!/bin/sh
  
 -if [ $(uname) = "Linux" ]; then
@@ -104,9 +49,32 @@ index 1d3ccf0..4b6e906 100755
 -fi
 -
 -BASEDIR=`dirname $($pathCmd $0)`
-+BASEDIR=`dirname $0`/`readlink $0`
-+BASEDIR=`dirname $(readlink $BASEDIR)`
++BASEDIR=`whereis -q $0 | xargs stat -f %R | xargs dirname`
  EXTLIBSDIR=$BASEDIR/extlibs
+ ASSEMBLY=$BASEDIR/target/scala-2.*/Princess-assembly*
  
- export CLASSPATH=$CLASSPATH:$BASEDIR/bin:$BASEDIR/parser/parser.jar:$EXTLIBSDIR/java-cup-11a.jar
-
+diff --git a/princessClient b/princessClient
+index f6e5d51..9411bfd 100755
+--- a/princessClient
++++ b/princessClient
+@@ -29,7 +29,7 @@ startDaemon() {
+     if [ ! -f "$portfile" ]; then
+         touch "$lockfile"
+ 
+-        BASEDIR=`dirname $($pathCmd $0)`
++        BASEDIR=`whereis -q $0 | xargs stat -f %R | xargs dirname`
+         EXTLIBSDIR=$BASEDIR/extlibs
+         ASSEMBLY=$BASEDIR/target/scala-2.*/Princess-assembly*
+ 
+diff --git a/princessGui b/princessGui
+index 74d70f7..8b886c3 100755
+--- a/princessGui
++++ b/princessGui
+@@ -1,6 +1,6 @@
+ #!/bin/sh
+ 
+-BASEDIR=`dirname $(readlink -e $0)`
++BASEDIR=`whereis -q $0 | xargs stat -f %R | xargs dirname`
+ EXTLIBSDIR=$BASEDIR/extlibs
+ ASSEMBLY=$BASEDIR/target/scala-2.*/Princess-assembly*
+ 
