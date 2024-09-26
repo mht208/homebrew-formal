80f2d6e0ce708dfa56e66990ba6827559deb4c18
Luca Di Stefano <lucadistefano90@gmail.com>
2024-09-26 10:11:38 +0200

    kissat: add 4.0.1 bottle.

--- kissat.rb
+++ kissat.rb
@@ -4,6 +4,12 @@ class Kissat < Formula
   url "https://github.com/arminbiere/kissat/archive/refs/tags/rel-4.0.1.tar.gz"
   sha256 "4b41edf12ffa5f8e8b1986e5ad3e0bedb4d34b0ed3ecc7c13362bc7ba0aba66b"
 
+  bottle do
+    root_url "https://github.com/lou1306/homebrew-formal/releases/download/kissat-4.0.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "913368e477a66cd4101acdca2137f93d1121c35ce9be69884049c75f9d4d5bf2"
+  end
+
   option "with-test", "Run test suite"
 
   def install
