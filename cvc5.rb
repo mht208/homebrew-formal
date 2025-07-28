2022c72eb2014a5dadbf52fc97c2d5d215f50e7e
Ming-Hsien Tsai <mhtsai208@gmail.com>
2025-07-28 15:44:13 +0800

    Add cvc5.

--- cvc5.rb
+++ cvc5.rb
@@ -1,23 +1,33 @@
 class Cvc5 < Formula
   desc "Efficient open-source automatic theorem prover for SMT problems"
   homepage "https://cvc5.github.io"
-  version "1.0.3"
+  version "1.3.0"
+
+  depends_on 'gmp'
+  depends_on 'cadical'
 
   # frozen_string_literal: true
   #
   if Hardware::CPU.intel?
-    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/cvc5-macOS"
-    sha256 "dca671880681ab51e544eab015347f7568174418bb58a22cebe3cefc007120ac"
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.3.0/cvc5-macOS-x86_64-static.zip"
+    sha256 "9bd61fbabd4786aca0c1bb63758fef56032b7e45a87880638073ec3cee7e6b2d"
   else
-    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.0.3/cvc5-macOS-arm64"
-    sha256 "00b29781288f4fca797d0fda3edc887cbf8b07f857d0e45ef567b0e512002336"
+    url "https://github.com/cvc5/cvc5/releases/download/cvc5-1.3.0/cvc5-macOS-arm64-static.zip"
+    sha256 "eb3c1a75efe28a7be7d05c4dd45846f0a7ab5cd1fcb7fb6987ebcac9164037bf"
   end
 
   def install
     if Hardware::CPU.intel?
-      bin.install "cvc5-macOS" => "cvc5"
+      bin.install "bin/cvc5"
+      include.install "include/cvc5"
+      lib.install "lib/libcvc5.a", "lib/libcvc5jni.dylib", "lib/libcvc5parser.a", "lib/libpicpoly.a", "lib/libpicpolyxx.a"
+      share.install Dir["share/*"]
     else
-      bin.install "cvc5-macOS-arm64" => "cvc5"
+      bin.install "bin/cvc5"
+      include.install "include/cvc5"
+      lib.install "lib/cmake"
+      lib.install "lib/libcvc5.a", "lib/libcvc5jni.dylib", "lib/libcvc5parser.a", "lib/libpicpoly.a", "lib/libpicpolyxx.a"
+      share.install Dir["share/*"]
     end
   end
 end
